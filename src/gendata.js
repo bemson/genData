192a3dc8fb775356e90a68f84dedc0ab2aca9100
Bemi Faison <bemson@gmail.com>
2011-07-30 04:50:13 -0700

    Removed proto-chaining flag, and added jsdoc comments
    As a constructor, this now prototypes as expected - no more special
    instantiation flags. Using jsdocs for comments, in hopes of rendering
    some quick API. Plus, removed (most) inline comments - may employ them
    again. Shaved a byte or two from small rewrite. Restructured code in
    preparation of linting.

--- src/gendata.js
+++ src/gendata.js
@@ -1,144 +1,209 @@
-/*
-genData is an object iterator and parser, with prototype-able curry functions
-
-Generator returns a dataset (an array of sequenced data points)
-1 - genData([stuff [, parsers [, model]]])
-  stuff - Mixed, any object to convert into a dataset
-  parsers - Array|Function, one or more parser functions (to modify data structure and control the parse loop)
-  model - Object, prototype for generated data
-
-Constructor returns a new generator that is part of the prototype chain and a preset parsers
-1 - new genData([parser1, [parserN, ...]])
-  parserN - Function, data constructor and dataset filters
-2 - new genData(model, parsers)
-  model - Object, prototype for generated data
-  parsers - Array, collection of parser functions (data constructors and dataset filters)
-
-Parser function signature:
-  name - String, the data name
-  value - Mixed, the data value
-  parent - Object, reference to the parent data (the "_OMIT" property indicates when the parent is not in the dataset)
-  datset - Array, the dataset being generated
-  flags - Object, collection of loop control flags
-    flags.omit - Bol, (default false), Indicates when this data object should be excluded from the dataset (after parsing completes)
-    flags.scan - Bol, (default true) Indicates when the value of this data object should be processed
-    flags.exit - Bol, (default false) Indicates when genData should stop parsing
-*/
+/*!
+ * genData @ https://github.com/bemson/genData
+ * by Bemi Faison
+ * MIT license
+ * **genData** generates normalized datasets and functions that encapsulate customizations while extending the prototype-chain.
+ */
+/**
+ * Define genData in the global scope.
+ *
+ * This function forks it's behavior when called with the `new` statement.
+ */
 function genData(stuff) {
-  // init vars
-  var args = arguments, // alias arguments
-    flags, // indicators for whether a data object is included in the dataset and parsed
-    args, // parser arguments
-    origFnc = args.callee, // this genData function
-    i, j, d, // loop vars
-    parsers = [], // array of parser functions
-    dataset = [], // dataset to return
-    tmpQ, // child queue for children
-    queue, // queue for creating data
-    qItem, // item from queue array
-    dataModel = origFnc, // constructor to prototype generated data - set to self by default
-    data; // data to assess with parsers (during loop)
-  // if not called with `new`...
-  if (!(this.hasOwnProperty && this instanceof origFnc)) {
-    // if given a second argument, set as parser value - wrap in array when a lone function
-    if (args[1]) parsers = typeof args[1] === 'function' ? [args[1]] : args[1];
-    // capture parsers length
-    j = parsers.length;
-    // define base data constructor
+/**
+ * Declare all variables, most of which are placeholders.
+ */
+  var args = arguments,
+    origFnc = args.callee,
+    parserFlags,
+    currentParserIndex,
+    totalParsers,
+    memberName,
+    parsers = [],
+    dataset = [],
+    queue,
+    queueBuffer,
+    queueItem,
+    dataModel = origFnc,
+    dataInstance;
+/**
+ * When invoked without the `new` operator, genData presumes it will normalize `stuff` and return a dataset.
+ */
+  if (!(this instanceof origFnc)) {
+    /**
+     * Handles the following argument signatures:
+     *
+     * 1. `genData([stuff])`
+     * 2. `genData(stuff, parser)`
+     * 3. `genData(stuff, parser, dataModel)`
+     * 4. `genData(stuff, parsersArray)`
+     * 5. `genData(stuff, parsersArray, dataModel)`
+     */
+    if (args[1]) {
+      parsers = typeof args[1] === 'function' ? [args[1]] : args[1];
+    }
+    totalParsers = parsers.length;
+    if (args[2]) {
+      dataModel = args[2];
+    }
+    /**
+     * Define an anonymous Data constructor.
+     *
+     * Set the constructor's prototype to the resolved dataModel (genData by default).
+     *
+     * For completeness, match the prototype's constructor to it's prototype function.
+     */
     function Data(name, value) {
       this.name = name;
       this.value = value;
     }
-    // if second argument is given, assume it's an object model (instance or constructor function)
-    if (args[2]) dataModel = args[2];
-    // set prototype to dataModel
     Data.prototype = dataModel.prototype;
-    // set constructor to dataModel
     Data.prototype.constructor = dataModel;
 
-    // queue stuff
-    queue = [['', stuff]]; // initial data point has no name or parent
-    // while there is a queue...
+    /**
+     * Queue an array of parameters, needed to instantiate a Data object.
+     */
+    queue = [
+      [
+        '', // _name_ passed to "new Data()"
+        stuff // _value_ passed to "new Data()"
+      ]
+    ];
+    /**
+     * Begin the main processing/normalization loop, and continue until `queue` is empty.
+     *
+     * Each loop begins with removing an item from the queue, defining a Data instance from it, and resetting loop variables - used when invoking each parser function.
+     */
     while (queue.length) {
-      // remove item first item from queue
-      qItem = queue.shift();
-      // reset loop var
-      i = 0;
-      // initialize data for this queued item - name and value
-      data = new Data(qItem[0], qItem[1]);
-      // reset flags
-      flags = {
-        omit: 0, // include data in dataset, by default
-        scan: 1, // scan value, by default
-        exit: 0 // do not exit, by default
+      queueItem = queue.shift();
+      dataInstance = new Data(queueItem[0], queueItem[1]);
+      currentParserIndex = 0;
+      /**
+       * Reset the flags object passed to _parser_ functions.
+       *
+       * - **omit**: When truthy, the data object is excluded from the dataset
+       * - **scan**: When falsy, members of the data object are not processed
+       * - **exit**: When truthy, genData stops processing the queue
+       */
+      parserFlags = {
+        omit: 0, // false, by default
+        scan: 1, // truthy, by default
+        exit: 0 // falsy, by default
       };
-      // cache arguments to parse data
-      args = [data.name, data.value, qItem[2], dataset, flags];
-      // process all parsers until parsing completes or is stopped...
-      while (i < j && !flags.exit) {
-        parsers[i++].apply(data, args);
+      /**
+       * Define the arguments for all _parsers_.
+       *
+       * 1. Original data name
+       * 2. Original data value
+       * 3. The parent data object (if any)
+       * 4. The dataset array
+       * 5. The flags object
+       */
+      args = [
+        dataInstance.name, // a string
+        dataInstance.value, // mixed type
+        queueItem[2], // a data object
+        dataset, // an array
+        parserFlags // an object
+      ];
+      /**
+       * Now pass arguments to each parser, unless one trips the "exit" flag.
+       */
+      while (currentParserIndex < totalParsers && !parserFlags.exit) {
+        parsers[currentParserIndex++].apply(dataInstance, args);
       }
-      // if excluding data...
-      if (flags.omit) {
-        // set omission flag to true (in case included child data references this data object)
-        data._OMIT = true;
-      } else { // otherwise, when not excluding this data object...
-        // add to dataset
-        dataset.push(data);
+      /**
+       * Observe the "omit" flag, to determine whether `data` will be returned with the dataset.
+       *
+       * (Excluded data objects are tagged, in case they are referenced later.)
+       */
+      if (parserFlags.omit) {
+        dataInstance._OMIT = true;
+      } else {
+        dataset.push(dataInstance);
       }
       // if exiting...
-      if (flags.exit) {
+      /**
+       * Observe the "exit" flag, which - when truthy - aborts processing the queue...
+       */
+      if (parserFlags.exit) {
         // clear the queue
         queue = [];
-      } else { // otherwise, when not exiting the iterator...
-        // reset temporary queue
-        tmpQ = [];
-        // if value may be scanned and this data's (final) value is an object...
-        if (flags.scan && typeof data.value === 'object') {
-          // with each property...
-          for (d in data.value) {
-            // if not inherited...
-            if (data.value.hasOwnProperty(d)) {
-              // add to temporary queue
-              tmpQ.push([d, data.value[d], data]);
+      }
+      /**
+       * If falsy, we next observe the "scan" flag, queuing parameters of future Data objects, for each non-inherited member from the current data object's value.
+       *
+       * A temporary buffer queues these members in first-to-last order.
+       */
+      else {
+        queueBuffer = [];
+        if (parserFlags.scan && typeof dataInstance.value === 'object') {
+          for (memberName in dataInstance.value) {
+            if (dataInstance.value.hasOwnProperty(memberName)) {
+              queueBuffer.push([
+                memberName, // _name_ passed to "new Data()"
+                dataInstance.value[memberName], // _value_ passed to "new Data()"
+                dataInstance // _parent_ (argument [2]) passed to parsers
+              ]);
             }
           }
         }
-        queue = tmpQ.concat(queue);
+        queue = queueBuffer.concat(queue);
       }
+    /**
+     * Wrap up the queue-loop, and return the resulting dataset
+     */
     }
-    // return final dataset
     return dataset;
-  } else if (stuff !== origFnc) { // or, when called with `new` and the first argument is not the origFnc...
-    // if second argument is an array...
-    if (args[1] instanceof Array) {
-      // use first argument as prototype (assume a constructor function)
+  }
+  /**
+   * When called with `new` and passed an argument, genData returns a _generator_, or curried function.
+   */
+  else if (stuff) {
+    /**
+     * Handles the following argument signatures:
+     *
+     * 1. `new genData(dataModel, parsersArray)`
+     * 2. `new genData(parser1 [, parserN])`
+     */
+    if (~{}.toString.call(args[1]).indexOf('y')) {
       dataModel = stuff;
-      // re-assign args to second argument (assumes an array of parser-functions)
       args = args[1];
     }
-    // add additional functions to existing parsers - skip check to ensure only functions are passed
     parsers = parsers.concat([].slice.call(args));
-    // return generator for this data model/parser combination (a curried call to the original genData function)
-    function Model(stuff, oParsers, model) {
-      // if called without `new` operator...
-      if (!(this.hasOwnProperty && this instanceof Model)) {
-        // return filtered/generated dataset - allow parser additions and overriding the datamodel
-        return origFnc(stuff, parsers.concat(oParsers || []), model && model.protoype ? model : Model);
-      } else if (stuff !== origFnc) { // or, when called with new operator and stuff is not the original function...
-        // return new Model generator with any added parsers
-        return new origFnc(Model, parsers.concat([].slice.call(arguments)));
+    /**
+     * Define a curry-like function that mimics genData's forking logic.
+     * Ultimately, this curry-like function calls genData to do the actual normalization.
+     */
+    function Generator(stuff, moreParsers, newModel) {
+      var moreParsersIsArray = ~{}.toString.call(moreParsers).indexOf('y');
+      if (!(this instanceof Generator)) {
+        return origFnc(
+          stuff,
+          parsers.concat(moreParsers || []),
+          newModel ? newModel : Generator
+        );
+      } else if (stuff) {
+        return new origFnc(
+          moreParsersIsArray ? stuff : Generator,
+          parsers.concat(
+            moreParsersIsArray ? 
+            moreParsers :
+            [].slice.call(arguments)
+          )
+        );
       }
-      // (otherwise) return self for prototyping
       return this;
     }
-    // add prototype chain of dataModel constructor
-    Model.prototype = new dataModel(origFnc);
-    // reset constructor
-    Model.prototype.constructor = Model;
-    // return new generator
-    return Model;
+    /**
+     * Chain this function to a given prototype, set it's constructor, and return it as a genData _generator_.
+     */
+    Generator.prototype = new dataModel();
+    Generator.prototype.constructor = dataModel;
+    return Generator;
   }
-  // return self for prototype-chaining
+  /**
+   * Finally, if invoked with `new` but given no arguments, we return an instance of genData. (This signature would be used for prototyping chaining.)
+   */
   return this;
 }
